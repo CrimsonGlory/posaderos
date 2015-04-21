07ba5c4096ba69bc04bb0152f365cf99c3f2d96e
Goolsc <lgdelorenzi@gmail.com>
2015-04-21 11:54:10 -0300

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -40,6 +40,27 @@ class PeopleTableSeeder extends Seeder {
 			'address'=>'Callao 504', 'gender'=>'Female',
 			'dni'=>123455,'other'=>'algun otro dato'
 			]);
+		Person::create(['first_name'=>'Ronaldinho',
+		 'last_name'=>'Gaucho',
+			'address'=>'Av. Brasil 789', 'gender'=>'Male',
+			'dni'=>30100123,'other'=>'Futbolista'
+			]);
+		Person::create(['first_name'=>'Mark',
+		 'last_name'=>'Zuckerberg',
+			'address'=>'Silicon Valley 123', 'gender'=>'Male',
+			'dni'=>30200123,'other'=>'Desarrollador'
+			]);
+		Person::create(['first_name'=>'Bill',
+		 'last_name'=>'Gates',
+			'address'=>'Long and winding road 777', 'gender'=>'Male',
+			'dni'=>30300123,'other'=>'CEO'
+			]);
+		Person::create(['first_name'=>'Jorge',
+		 'last_name'=>'Rial',
+			'address'=>'Callao 900', 'gender'=>'Male',
+			'dni'=>30400123,'other'=>'Actor'
+			]);
+		
 		
 		// $this->call('UserTableSeeder');
 	}
