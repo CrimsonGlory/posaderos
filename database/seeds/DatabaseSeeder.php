bfbaa1689e388da12067e95d94c38856a1639049
CrimsonGlory <javierbassi@gmail.com>
2015-04-21 23:16:35 -0300

    fixs to interactions seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 use App\Person;
+use App\Interaction;
+use App\User;
 class DatabaseSeeder extends Seeder {
 
 	/**
@@ -15,7 +17,6 @@ class DatabaseSeeder extends Seeder {
 		Model::unguard();
 
 		$this->call('PeopleTableSeeder');
-		$this->call('InteractionTableSeeder');
 		$this->call('UserTableSeeder');
 	}
 
@@ -24,81 +25,70 @@ class PeopleTableSeeder extends Seeder {
 
 	public function run()
 	{
+		DB::table('interactions')->delete();
 		DB::table('people')->delete();
-		Person::create(['first_name'=>'Juan Román',
+		DB::table('users')->delete();
+
+		$person1=Person::create(['first_name'=>'Juan Román',
 		 'last_name'=>'Riquelme',
 			'address'=>'Callao 500', 'gender'=>'Male']);
-		Person::create(['first_name'=>'Carlos',
+		$person2=Person::create(['first_name'=>'Carlos',
 		 'last_name'=>'Navarro Montoya',
 			'address'=>'Callao 501', 'gender'=>'Male']);
-		Person::create(['first_name'=>'Carlos',
+		$person3=Person::create(['first_name'=>'Carlos',
 		 'last_name'=>'Mac Allister',
 			'address'=>'Callao 502', 'gender'=>'Male']);
-		Person::create(['first_name'=>'Diego Armando',
+		$person4=Person::create(['first_name'=>'Diego Armando',
 		 'last_name'=>'Maradona',
 			'address'=>'Callao 503', 'gender'=>'Male']);
-		Person::create(['first_name'=>'Valeria',
+		$person5=Person::create(['first_name'=>'Valeria',
 		 'last_name'=>'Mazza',
 			'address'=>'Callao 504', 'gender'=>'Female',
 			'dni'=>123455,'other'=>'algun otro dato'
 			]);
-		Person::create(['first_name'=>'Ronaldinho',
+		$person6=Person::create(['first_name'=>'Ronaldinho',
 		 'last_name'=>'Gaucho',
 			'address'=>'Av. Brasil 789', 'gender'=>'Male',
 			'dni'=>30100123,'other'=>'Futbolista'
 			]);
-		Person::create(['first_name'=>'Mark',
+		$person7=Person::create(['first_name'=>'Mark',
 		 'last_name'=>'Zuckerberg',
 			'address'=>'Silicon Valley 123', 'gender'=>'Male',
 			'dni'=>30200123,'other'=>'Desarrollador'
 			]);
-		Person::create(['first_name'=>'Bill',
+		$person8=Person::create(['first_name'=>'Bill',
 		 'last_name'=>'Gates',
 			'address'=>'Long and winding road 777', 'gender'=>'Male',
 			'dni'=>30300123,'other'=>'CEO'
 			]);
-		Person::create(['first_name'=>'Jorge',
+		$person9=Person::create(['first_name'=>'Jorge',
 		 'last_name'=>'Rial',
 			'address'=>'Callao 900', 'gender'=>'Male',
 			'dni'=>30400123,'other'=>'Actor'
 			]);
 		
 		
-		// $this->call('UserTableSeeder');
-	}
-
-}
-class InteractionTableSeeder extends Seeder {
-	public function run(){
-		DB::table('interactions')->delete();
-		
-		Interaction::create(['text'=>'La persona recibio Atencion Medica',
-		 'date'=>'2015-4-20',
-			'person_id'=>30400123]);
-		Interaction::create(['text'=>'Persona con necesidad de obtener documento nacional de identidad',
-		 'date'=>'2015-4-20',
-			'person_id'=>30100123]);
-		Interaction::create(['text'=>'Persona asistio a comedor (Comedor X)',
-		 'date'=>'2015-4-20',
-			'person_id'=>30200123]);
-		Interaction::create(['text'=>'Se le dio ropa a la persona',
-		 'date'=>'2015-4-20',
-			'person_id'=>30300123]);
+		//Interactions
+		$person1->interactions()->save(new Interaction(['text'=>'La persona recibio Atencion Medica',
+		 'date'=>'2015-4-20']));
+		$person2->interactions()->save(new Interaction(['text'=>'Persona con necesidad de obtener documento nacional de identidad',
+		 'date'=>'2015-4-20']));
+		$person2->interactions()->save(new Interaction(['text'=>'Persona asistio a comedor (Comedor X)',
+		 'date'=>'2015-4-20']));
+		$person3->interactions()->save(new Interaction(['text'=>'Se le dio ropa a la persona',
+		 'date'=>'2015-4-20']));
 	}
 }
 class UserTableSeeder extends Seeder{
 	public function run(){
-		DB::table('users')->delete();
 		User::create(['name'=>'Luciano Delorenzi',
-		 'email'=>'lgdelorenzi@gmail.com',
-			'password'=>'delorenzi']);
+		 'email'=>'lgdelorenzi2@gmail.com',
+			'password'=>Hash::make('delorenzi')]);
 		User::create(['name'=>'Enzo Sagretti',
-		 'email'=>'enzosagretti@gmail.com',
-			'password'=>'sagretti']);
+		 'email'=>'enzosagretti2@gmail.com',
+			'password'=>Hash::make('sagretti')]);
 		User::create(['name'=>'Agustin Puentes',
-		 'email'=>'aguspuentes@hotmail.com',
-			'password'=>'puentes']);
-	}
-}
+		 'email'=>'aguspuentes2@hotmail.com',
+			'password'=>Hash::make('puentes')]);
 	}
 }
