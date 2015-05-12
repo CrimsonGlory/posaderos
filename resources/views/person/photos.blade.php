f2ca7382e7b30a05ecb4be57b15653d96b3c14ac
aguspuentes <aguspuentes@gmail.com>
2015-05-12 18:14:19 -0300

    Se corrige la vista de la galeria de fotos para que se ajuste a las pantallas de los dispositivos moviles.

--- resources/views/person/photos.blade.php
+++ resources/views/person/photos.blade.php
@@ -4,7 +4,7 @@
     <!--<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">-->
     <link rel="stylesheet" href="/css/blueimp-gallery.css">
     <link rel="stylesheet" href="/css/bootstrap-image-gallery.min.css">
-	<link rel="stylesheet" href="/css/reset.css">{{-- Sin esto se enciman los botones  --}}
+    <link rel="stylesheet" href="/css/reset.css">{{-- Sin esto se enciman los botones  --}}
 @endsection
 
 @section('content')
@@ -15,7 +15,7 @@
         <!-- Controls for the borderless lightbox -->
         <h3 class="title"></h3>
         <a class="prev">‹</a>
-	<a class="setavatar">x</a>
+        <a class="setavatar">x</a>
         <a class="next">›</a>
         <a class="close">×</a>
         <a class="play-pause"></a>
@@ -30,19 +30,29 @@
                     </div>
                     <div class="modal-body next"></div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-default pull-left prev">
-                            <i class="glyphicon glyphicon-chevron-left"></i>
-                            Anterior
-                        </button>{!! Form::open(['name'=>'id','class' => 'reset-this', 'method'=> 'POST', 'action' => ['PersonController@setAvatar',$person->id]]) !!}
-			<input type="hidden" name="fileentry_id"  value=""> 
-			<button type="button submit" class="btn btn-default setavatar">
-			<i class="glyphicon glyphicon-user"></i>
-			Elegir como foto de perifl
-			</button>{!! Form::close() !!}
-                        <button type="button" class="btn btn-primary next" >
-                           Siguiente 
-                            <i class="glyphicon glyphicon-chevron-right"></i>
-                        </button>
+                        <table width="100%">
+                            <tr>
+                                <td align="left">
+                                    <button type="button" class="btn btn-primary prev">
+                                        <i class="glyphicon glyphicon-chevron-left"></i>
+                                    </button>
+                                </td>
+                                <td align="middle">
+                                    {!! Form::open(['name'=>'id','class' => 'reset-this', 'method'=> 'POST', 'action' => ['PersonController@setAvatar',$person->id]]) !!}
+                                        <input type="hidden" name="fileentry_id"  value="">
+                                        <button type="button submit" class="btn btn-default setavatar">
+                                            <i class="glyphicon glyphicon-user"></i>
+                                            Usar como perfil
+                                        </button>
+                                    {!! Form::close() !!}
+                                </td>
+                                <td align="right">
+                                    <button type="button" class="btn btn-primary next">
+                                        <i class="glyphicon glyphicon-chevron-right"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        </table>
                     </div>
                 </div>
             </div>
@@ -63,15 +73,15 @@
     <script src="/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="/Gallery/js/jquery.blueimp-gallery.min.js"></script>
     <script src="/js/bootstrap-image-gallery.min.js"></script>
-<script type="text/javascript">
-$( "formm" ).submit(function(){
-	alert(2);
-   // Let's find the input to check
-   var $input = $(this).find("input[name=fileentry_id]");
-     // Value is falsey (i.e. null), lets set a new one
-     $input.val("whatever you want");
-});
-</script>
+    <script type="text/javascript">
+        $( "formm" ).submit(function(){
+            alert(2);
+            // Let's find the input to check
+            var $input = $(this).find("input[name=fileentry_id]");
+            // Value is falsey (i.e. null), lets set a new one
+            $input.val("whatever you want");
+        });
+    </script>
 
 @endsection
 
