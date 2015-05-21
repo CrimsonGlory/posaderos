66f3fcdcdb0959b12dac9d4e6a7258bddeb08018
aguspuentes <aguspuentes@gmail.com>
2015-05-21 14:57:58 -0300

    Cambios visuales menores en la pagina de busqueda.

--- resources/views/search/resultadoBusqueda.blade.php
+++ resources/views/search/resultadoBusqueda.blade.php
@@ -1,7 +1,9 @@
 @if ($data['error'] == 1)
-    <ul class="alert alert-danger">
-        <label>Error en la consulta: Falta parámetro de búsqueda</label>
-    </ul>
+    <div class="panel-body">
+        <ul class="alert alert-danger">
+            <label>Error en la consulta: Falta parámetro de búsqueda</label>
+        </ul>
+    </div>
 @elseif (!is_null($people))
     @include('person.list_people',['people' => $people])
 @elseif (!is_null($interactions))
