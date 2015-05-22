1671e25c231874cd1163a204ed15f02512e176b5
aguspuentes <aguspuentes@gmail.com>
2015-05-22 16:45:49 -0300

    Se corrige la busqueda de asistidos para que se pueda buscar por parte del DNI. Ademas, se filtra la busqueda por numeros dependiendo de los permisos del usuario.

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -49,57 +49,79 @@ class SearchController extends Controller {
         $people = NULL;
         $users = NULL;
 
-        if($data['toFind'] == "Asistidos")
+        if ($data['keyWord'] != '')
         {
-	    if($data['keyWord'] ==  preg_replace("/[^0-9,.-_ +]/", "", $data['keyWord'])){ //phone or dni
-		$number=preg_replace("/[^0-9]/","",$data['keyWord']);
-		$people = Person::where('dni','=',$number)->
-				  orWhere('phone','=',parse_phone($number))->
-				  orderBy('id','desc')->limit(30)->get();
-	    }
-            else if ($user->can('see-all-people'))
+            if($data['toFind'] == "Asistidos")
             {
-                $people = Person::where(DB::raw('concat_ws(\' \',first_name,last_name)'),'LIKE','%'.$data['keyWord'].'%')->
-				  orWhere('first_name', 'LIKE', '%'.$data['keyWord'].'%')->
-                                  orWhere('last_name', 'LIKE', '%'.$data['keyWord'].'%')->
-				  orWhere('address','LIKE','%'.$data['keyWord'].'%')->
-                                  orderBy('id', 'desc')->limit(30)->get();
+                if ($user->can('see-all-people'))
+                {
+                    if($data['keyWord'] ==  preg_replace("/[^0-9,.-_ +]/", "", $data['keyWord'])) // phone or dni
+                    {
+                        $number = preg_replace("/[^0-9]/","",$data['keyWord']);
+                        $people = Person::where(DB::raw('concat_ws(\' \',dni)'),'LIKE','%'.$data['keyWord'].'%')->
+                                          orWhere('dni','LIKE',$number)->
+                                          orWhere('phone','=',parse_phone($number))->
+                                          orderBy('id','desc')->limit(30)->get();
+                    }
+                    else // first_name, last_name or address
+                    {
+                        $people = Person::where(DB::raw('concat_ws(\' \',first_name,last_name)'),'LIKE','%'.$data['keyWord'].'%')->
+                                          orWhere('first_name', 'LIKE', '%'.$data['keyWord'].'%')->
+                                          orWhere('last_name', 'LIKE', '%'.$data['keyWord'].'%')->
+                                          orWhere('address','LIKE','%'.$data['keyWord'].'%')->
+                                          orderBy('id', 'desc')->limit(30)->get();
+                    }
+
+                }
+                else if ($user->can('see-new-people'))
+                {
+                    if($data['keyWord'] ==  preg_replace("/[^0-9,.-_ +]/", "", $data['keyWord'])) // phone or dni
+                    {
+                        $number = preg_replace("/[^0-9]/","",$data['keyWord']);
+                        $people = Person::where('created_by', $user->id)->
+                                          where(function ($query) use($data, $number){
+                                              $query->where(DB::raw('concat_ws(\' \',dni)'),'LIKE','%'.$data['keyWord'].'%')->
+                                              orWhere('dni','LIKE',$number)->
+                                              orWhere('phone','=',parse_phone($number));
+                                          })->orderBy('id','desc')->limit(30)->get();
+                    }
+                    else // first_name, last_name or address
+                    {
+                        $people = Person::where('created_by', $user->id)->
+                                          where(function ($query) use($data){
+                                              $query->where(DB::raw('concat_ws(\' \',first_name,last_name)'),'LIKE','%'.$data['keyWord'].'%')->
+                                              orWhere('first_name', 'LIKE', '%'.$data['keyWord'].'%')->
+                                              orWhere('last_name', 'LIKE', '%'.$data['keyWord'].'%')->
+                                              orWhere('address','LIKE','%'.$data['keyWord'].'%');
+                                          })->orderBy('id', 'desc')->limit(30)->get();
+                    }
+                }
+                $data['error'] = 0;
             }
-            else if ($user->can('see-new-people'))
+            else if($data['toFind'] == "Interacciones")
             {
-                $people = Person::where('created_by', $user->id)->
-                                  where(function ($query) use($data){
-                                        $query->where(DB::raw('concat_ws(\' \',first_name,last_name)'),'LIKE','%'.$data['keyWord'].'%')->
-	                                  orWhere('first_name', 'LIKE', '%'.$data['keyWord'].'%')->
-	                                  orWhere('last_name', 'LIKE', '%'.$data['keyWord'].'%')->
-       		                           orWhere('address','LIKE','%'.$data['keyWord'].'%');
-                                  })->orderBy('id', 'desc')->limit(30)->get();
+                if ($user->can('see-all-interactions'))
+                {
+                    $interactions = Interaction::where('text', 'LIKE', '%'.$data['keyWord'].'%')->orderBy('id', 'desc')->limit(30)->get();
+                }
+                else if ($user->can('see-new-interactions'))
+                {
+                    $interactions = Interaction::where('user_id', $user->id)->
+                    where('text', 'LIKE', '%'.$data['keyWord'].'%')->
+                    orderBy('id', 'desc')->limit(30)->get();
+                }
+                $data['error'] = 0;
             }
-            $data['error'] = 0;
-        }
-        else if($data['toFind'] == "Interacciones")
-        {
-            if ($user->can('see-all-interactions'))
+            else if($data['toFind'] == "Usuarios")
             {
-                $interactions = Interaction::where('text', 'LIKE', '%'.$data['keyWord'].'%')->orderBy('id', 'desc')->limit(30)->get();
+                if ($user->can('see-users'))
+                {
+                    $users = User::where('name', 'LIKE', '%'.$data['keyWord'].'%')->
+                    orWhere('email', 'LIKE', '%'.$data['keyWord'].'%')->
+                    orderBy('id', 'desc')->limit(30)->get();
+                }
+                $data['error'] = 0;
             }
-            else if ($user->can('see-new-interactions'))
-            {
-                $interactions = Interaction::where('user_id', $user->id)->
-                                             where('text', 'LIKE', '%'.$data['keyWord'].'%')->
-                                             orderBy('id', 'desc')->limit(30)->get();
-            }
-            $data['error'] = 0;
-        }
-        else if($data['toFind'] == "Usuarios")
-        {
-            if ($user->can('see-users'))
-            {
-                $users = User::where('name', 'LIKE', '%'.$data['keyWord'].'%')->
-                               orWhere('email', 'LIKE', '%'.$data['keyWord'].'%')->
-                               orderBy('id', 'desc')->limit(30)->get();
-            }
-            $data['error'] = 0;
         }
         return view('search.resultadoBusqueda', compact('data','people','interactions','users'));
     }
