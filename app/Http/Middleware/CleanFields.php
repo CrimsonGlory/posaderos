edb6b76e30ffb31fab67ed6cabce1f977bbc67d8
CrimsonGlory <javierbassi@gmail.com>
2015-05-27 00:16:48 -0300

    limpio campos first_name last_name dni address name

--- app/Http/Middleware/CleanFields.php
+++ app/Http/Middleware/CleanFields.php
@@ -16,6 +16,18 @@ class CleanFields {
 		if($request->first_name != NULL){
 			$request->merge(array('first_name' => $this->clean($request->first_name)));
 		}
+		if($request->last_name != NULL){
+			$request->merge(array('last_name' => $this->clean($request->last_name)));
+		}
+		if($request->dni != NULL){
+                        $request->merge(array('dni' => $this->clean($request->dni)));
+                }
+		if($request->address != NULL){
+                        $request->merge(array('last_name' => $this->clean($request->last_name)));
+                }
+		if($request->name != NULL){
+                        $request->merge(array('name' => $this->clean($request->name)));
+                }
 		return $next($request);
 	}
 	private function clean($string){
