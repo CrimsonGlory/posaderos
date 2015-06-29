22893b4fe9947788b95b57a8fe558b7bcd8641fa
CrimsonGlory <javierbassi@gmail.com>
2015-06-28 21:24:06 -0300

    #63 disable normal registration until setup is complete. redirect to setup

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -35,7 +35,8 @@ class AuthController extends Controller {
 		$this->auth = $auth;
 		$this->registrar = $registrar;
 
-		$this->middleware('guest', ['except' => 'getLogout']);
+        $this->middleware('guest', ['except' => 'getLogout']);
+        $this->middleware('check.setup');
 	}
 
     public function postRegister(Request $request)
