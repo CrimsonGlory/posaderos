d3604cdfecadd4b7753e586fa2d176000ea07f7b
CrimsonGlory <javierbassi@gmail.com>
2015-08-08 14:07:00 -0300

    #132 header botonera usuario: 1

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -128,7 +128,7 @@
                         <li><a href="{{ url('/auth/register') }}">{{ trans('messages.register') }}</a></li>
                     @else
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" style="color: #ffffff">
                                 <img src="{{ Auth::user()->gravatar() }}" class="img-circle" alt="" onerror="this.src='{{ asset("no-photo.png") }}';" style="width: 20px; height: 20px;"> {{ Auth::user()->name }}
                                 <span class="caret"></span>
                             </a>
